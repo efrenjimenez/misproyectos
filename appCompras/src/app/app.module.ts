d572c39f82e373dadd5592749d9f1909659be85d
efrenjimenez <efrenjimenezcarrion@gmail.com>
2022-02-16 10:51:12 +0100

    creacion servicios

--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
